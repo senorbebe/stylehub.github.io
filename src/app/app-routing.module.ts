3fd62f54c2eef9e20863721c36277cc12d3c7a6d
Nicolas Barg <nicolas.barg@nordakademie.de>
2022-02-19 18:48:57 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductsComponent} from "./products/products.component";
+import {ImprintComponent} from "./imprint/imprint.component";
+import {PrivacyPolicyComponent} from "./privacy-policy/privacy-policy.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home-component', component: HomeComponent},
+  {path: 'products-component', component: ProductsComponent},
+  {path: 'imprint-component', component: ImprintComponent},
+  {path: 'privacy-policy-component', component: PrivacyPolicyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
