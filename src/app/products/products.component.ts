0a59744b6d10b07d6e985cb30a19c88557ab64b1
Jonas Haupert <jonas.haupert@nordakademie.de>
2022-02-21 01:39:11 +0100

    Vertically center text in cards

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,19 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
-  show: boolean = true;
+export class ProductsComponent {
   div = -1;
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
 
   showDetails(index: number) {
     this.div = index;
