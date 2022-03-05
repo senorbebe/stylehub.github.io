7942bb951c71fa5dc8a60246e5df61f03774f15a
Nicolas Barg <nicolas.barg@nordakademie.de>
2022-03-05 13:05:07 +0100

    routing fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,8 @@ import {ContactComponent} from "./contact/contact.component";
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'produkte', component: ProductsComponent},
-  {path: 'imprint-component', component: ImprintComponent},
-  {path: 'privacy-policy-component', component: PrivacyPolicyComponent},
+  {path: 'impressum', component: ImprintComponent},
+  {path: 'datenschutz', component: PrivacyPolicyComponent},
   {path: 'nachhaltigkeit', component: SustainabilityComponent},
   {path: 'kontakt', component: ContactComponent},
   {path: '**', redirectTo: 'home', pathMatch: 'full'}
